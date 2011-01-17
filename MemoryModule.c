0ddce45dc506afe79d095f21ee815e6892e040af
Joachim Bauch <mail@joachim-bauch.de>
2011-01-17 21:25:27 +0100

    fixed compilation with Vista SDK where "IMAGE_SIZEOF_BASE_RELOCATION" is no longer defined (GitHub issue #1)

--- MemoryModule.c
+++ MemoryModule.c
@@ -33,6 +33,11 @@
 #include <stdio.h>
 #endif
 
+#ifndef IMAGE_SIZEOF_BASE_RELOCATION
+// Vista SDKs no longer define IMAGE_SIZEOF_BASE_RELOCATION!?
+#define IMAGE_SIZEOF_BASE_RELOCATION (sizeof(IMAGE_BASE_RELOCATION))
+#endif
+
 #include "MemoryModule.h"
 
 typedef struct {
