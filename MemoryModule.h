d0cf3a2084cbe7bfcff474508de7867133ece119
Joachim Bauch <mail@joachim-bauch.de>
2012-06-28 19:42:15 +0200

    updated license to MPL 2.0

--- MemoryModule.h
+++ MemoryModule.h
@@ -6,7 +6,7 @@
  * http://www.joachim-bauch.de
  *
  * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
+ * 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
