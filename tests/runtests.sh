d9dc0b02709ae1fff65e7d0183a7d8fe17965272
Joachim Bauch <mail@joachim-bauch.de>
2015-03-01 01:16:33 +0100

    Setup additional environment flags for wine.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-if [ "$1" = "x86_64" ]; then
+PLATFORM=$1
+if [ "${PLATFORM}" = "x86_64" ]; then
     export WINEPREFIX=${HOME}/.wine64/
+    WINE=wine64
 else
     export WINEPREFIX=${HOME}/.wine/
+    WINE=wine
 fi
+export WINEPATH=/usr/lib/gcc/${PLATFORM}-w64-mingw32/4.6/
 
 read -a TEST_DLLS <<< $2
 
@@ -11,7 +15,7 @@ for filename in "${TEST_DLLS[@]}"
 do
     :
     echo "Testing $filename"
-    ./LoadDll.exe $filename
+    ${WINE} ./LoadDll.exe $filename
     if [ "$?" != "0" ]; then
         exit 1
     fi
