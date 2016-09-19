097403a8addca6553355484836c4aa7941c8f1c6
Joachim Bauch <mail@joachim-bauch.de>
2016-09-20 00:14:56 +0200

    Added wrapper to run wine/wine64.

---
+++ tests/runwine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+PLATFORM=$1
+shift
+
+if [ "${PLATFORM}" = "x86_64" ]; then
+    export WINEPREFIX=${HOME}/.wine64/
+    WINE=wine64
+else
+    export WINEPREFIX=${HOME}/.wine/
+    WINE=wine
+fi
+export WINEPATH=/usr/lib/gcc/${PLATFORM}-w64-mingw32/4.6/
+
+exec ${WINE} $@
